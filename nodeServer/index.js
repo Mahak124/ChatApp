1f119936aa250d3a77ee5fc618a42441102422b3
Mahak <pathakmahak64@gmail.com>
2023-07-04 10:40:05 +0530

    initial commit

---
+++ nodeServer/index.js
@@ -0,0 +1,25 @@
+// Node Server which will handle socket io connections
+const io = require('socket.io')(800)
+
+const users = {}
+
+io.on('connection' , socket =>{
+
+    // if any new user join , let other users connected to the server know
+    socket.on('new-user-joined' , name =>{
+        users[socket.id] = name
+        socket.broadcast.emit('user-joined' , name)
+        // console.log("new-user-joined" , name);
+    });
+
+    // if someone sends a message , broadcast it to other people
+    socket.on('send' , message =>{
+        socket.broadcast.emit('receive' , {message: message , name: users[socket.id]})
+    });
+
+    // if someone leaves the chat , let others know...
+    socket.on('disconnect' , message=>{
+        socket.broadcast.emit('left' , users[socket.id])
+        delete users[socket.id];
+    });
+})
